12f1c5535692b5cc0668dc800bb93655370a676b
Julián Almada <juliiialmada13@gmail.com>
2026-04-27 16:36:11 -0300

    Add files via upload

---
+++ WebAppParcial1/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAppParcial1.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ WebAppParcial1/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAppParcial1.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int CategoriaId { get; set; }
+        public double Precio { get; set; }
+        public string Marca { get; set; }
+        public bool Favorito { get; set; }
+
+        public Categoria? categoria { get; set; }
+
+    }
+}
