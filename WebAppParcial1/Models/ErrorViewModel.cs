dee870818bb04f7735e6592a851c4aa73a7085ee
UTN <UTN@ACADEMICA-02>
2026-04-27 14:38:05 -0300

    Agregar archivos de proyecto.

---
+++ WebAppParcial1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppParcial1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
