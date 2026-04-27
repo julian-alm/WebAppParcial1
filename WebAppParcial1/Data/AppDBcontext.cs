4dc5b6c7b5a906642b0dcb9d7b7e157ea12f4ccf
Julián Almada <juliiialmada13@gmail.com>
2026-04-27 16:44:55 -0300

    Add files via upload

---
+++ WebAppParcial1/Data/AppDBcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppParcial1.Models;
+
+namespace WebAppParcial1.Data
+{
+    public class AppDBcontext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=ProductosParcial1;Trusted_Connection=True;MultipleActiveResultSets=True");
+        }
+
+        public DbSet<Categoria> categoria { get; set; }
+        public DbSet<Producto> productos { get; set; }
+    }
+}
