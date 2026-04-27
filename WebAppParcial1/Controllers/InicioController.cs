f89be2bcbff03dbde036a5742ea2d83d66758f68
Julián Almada <juliiialmada13@gmail.com>
2026-04-27 16:35:01 -0300

    Add files via upload

---
+++ WebAppParcial1/Controllers/InicioController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppParcial1.Data;
+
+namespace WebAppParcial1.Controllers
+{
+    public class InicioController : Controller
+    {
+        private readonly AppDBcontext _context;
+
+        public InicioController()
+        {
+            _context = new AppDBcontext();
+        }
+
+        // GET: Productos
+        public async Task<IActionResult> Index()
+        {
+            var appDBcontext = _context.productos.Include(p => p.categoria);
+            return View(await appDBcontext.ToListAsync());
+        }
+    }
+}
